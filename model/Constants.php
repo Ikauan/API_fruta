9b80046a33b8b4de34e08d9914c291a0ca8f8746
Ikauan <86685099+Ikauan@users.noreply.github.com>
2022-03-29 15:52:01 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'kauan.mysql.database.azure.com');
+define('DB_USER', 'Kauan@kauan');
+define('DB_PASS', 'caminhao321@');
 define('DB_NAME', 'hortifrutibd');
